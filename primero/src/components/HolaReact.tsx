e477a41c8d51d7d60bcd0ccca3e9cc43d3d8a360
esuaresu <esuaresu@gmail.com>
2026-01-22 12:34:48 +0100

    add HolaReact.tsk

---
+++ primero/src/components/HolaReact.tsx
@@ -0,0 +1,5 @@
+function HolaReact() {
+    let hola = "Hola DAW";
+    return<h1>{hola}</h1>;
+}
+export default HolaReact;
--- primero/src/main.tsx
+++ primero/src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './components/List.tsx'
+import HolaReact from './components/HolaReact.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <HolaReact />    
     <App />
     <App />
   </StrictMode>,
