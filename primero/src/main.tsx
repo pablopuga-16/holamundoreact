a9e7570d49812fe883c5f576d9a920853abf732d
esuaresu <esuaresu@gmail.com>
2026-02-19 12:40:14 +0100

    add function filter JSON

--- primero/src/main.tsx
+++ primero/src/main.tsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.css'
-import App from './components/List.tsx'
-import HolaReact from './components/HolaReact.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./components/List.tsx";
+import HolaReact from "./components/HolaReact.tsx";
+import Ods from "./components/Ods.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HolaReact />    
-    <App />
+    <HolaReact />
+    {/* <App /> */}
+    <Ods />
     <App />
   </StrictMode>,
-)
+);
